c0908aac1a95e670957603bb8feed5a8017190dc
pedrovitors <pedrovitorfhs@gmail.com>
2021-02-22 17:01:19 -0300

    Realizado exercicio da aula de hoje

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,96 @@
+/* 
+   Exercicio 1
+   a = 10
+   b = 10
+
+    console.log(b)
+// Vai mostrar a variavel 10
+
+    b = 5
+    console.log(a, b)
+// Vai mostrar a variavel 10 referente a variavel "a" e então trocar a variavel "b" e logar 5
+*/
+
+/*
+   Exercicio 2
+    a = 10
+    b = 20
+    c = a
+    b = c
+    a = b
+
+    console.log(a, b, c)
+// Vai mostrar as variaveis, em ordem (10, 10, 10)
+   let nome;
+   let idade;
+
+   typeof nome;
+   typeof idade;
+   console.log(typeof nome, typeof idade);
+// Ambas as variaveis foram dadas como "undefined", pois nenhuma delas tem um elemento atribuido
+*/
+
+/* Exercicio de Escrita de Código 1
+    let nome = prompt("Qual o seu nome?");
+    let idade = prompt("Qual a sua idade?");
+
+    console.log("Olá, " + nome, "voce tem "+ idade, "anos");
+
+    typeof nome;
+    typeof idade;
+    console.log(typeof nome, typeof idade);
+// Agora, as variaveis são do tipo string, ou seja elas tem um valor atribuidos a elas, e representam o texto (input do usuario).
+*/
+
+/* Exercicio de Escrita de Código 2
+ let endereco = prompt("Qual o seu endereço?");
+ let comida = prompt("Qual a sua comida favorita?");
+ let filme = prompt("Qual o seu filme preferido?");
+ let profissao = prompt("Qual a sua profissao?");
+ let participar = prompt("Gostaria de participar da iniciativa xxxx?");
+
+console.log("1. Qual o seu endereço?");
+console.log("Resposta:", endereco);
+
+console.log("2.Qual a sua comida favorita?");
+console.log("Resposta:", comida);
+
+console.log("3. Qual o seu filme preferido?");
+console.log("Resposta:", filme);
+
+console.log("4. Qual a sua profissao");
+console.log("Resposta:", profissao);
+
+console.log("5. Gostaria de participar da iniciativa xxxx");
+console.log("Resposta:", participar);
+*/
+
+/* Exercicio de Escrita de Código 3
+let Ramem = "Ramem";
+let Bugolgi = "Bugolgi";
+let Pizza = "Pizza";
+let Sushi = "Sushi";
+let Macarrao = "Macarrao";
+
+const comidas = [Ramem, Bugolgi, Pizza, Sushi, Macarrao];
+console.log(comidas);
+
+console.log("Essas são as minhas comidas preferidas:");
+
+console.log(Ramem);
+console.log(Bugolgi);
+console.log(Pizza);
+console.log(Sushi);
+console.log(Macarrao);
+
+//Desafio - Substituir "let Bugolgi = "Bugolgi"; por let Bugolgi = prompt("Qual a sua comida preferida?").
+Quando inserido o novo nome para a variavel, irá mudar o segundo item do array e da lista.
+ */
+
+/*Exercicio de Escrita de Código 4
+const perguntas = ["Você está usando uma roupa azul hoje?", "Voce está usando óculos?", "Voce já bebeu agua hoje?"];
+
+console.log(perguntas[0], true)
+console.log(perguntas[1], false)
+console.log(perguntas[2], true)
+ */
