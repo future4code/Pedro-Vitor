baafa4460e37935926acc95bdfbb97074bf0bae3
pedrovitors <pedrovitorfhs@gmail.com>
2021-03-24 16:24:07 -0300

    Exercicios realizados

--- semana7/integracao-de-api/src/Components/userList/UserList.js
+++ semana7/integracao-de-api/src/Components/userList/UserList.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import axios from "axios";
 
 const ListContainer = styled.div`
 display: flex;
@@ -7,14 +8,61 @@ flex-direction: column;
 align-items: center;
 text-align: center;
 `
-export default class UserList extends React.Component {
 
-    render(){
-        return(
-            <ListContainer>
-                <li> teste 1 </li>
-                <li> teste 2 </li>
-                <li> teste 3 </li>
+export default class UserList extends React.Component {
+    state = {
+        userList: []
+    }
+
+    componentDidMount() {
+        this.getUserList()
+    }
+
+    deleteUser = (id) => {
+        axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`,
+            {
+                headers: {
+                    Authorization: "pedro-vitor-cruz"
+                }
+            }
+        ).then(r => {
+            alert("Usuario excluido")
+            console.log(r)
+        }).catch(e => {
+            alert("Erro ao excluir usuario")
+            console.log(e)
+        })
+    }
+
+    getUserList = () => {
+        axios.get(
+            "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+            {
+                headers: {
+                    Authorization: "pedro-vitor-cruz"
+                }
+            }
+        ).then((r) => {
+            this.setState({userList: r.data})
+        })
+            .catch((err) => {
+                console.log(err.response.data)
+            })
+    }
+
+    render() {
+        const mapUsers = this.state.userList.map((user) => {
+            return (
+                <div key={user.id}>
+                    <li>{user.name}</li>
+                    <button onClick={() => this.deleteUser(user.id)}> Deletar usuario </button>
+                </div>
+            )
+        })
+
+        return (<ListContainer>
+                <h3> Usuarios Cadastrados </h3>
+                {mapUsers}
             </ListContainer>
         )
     }
